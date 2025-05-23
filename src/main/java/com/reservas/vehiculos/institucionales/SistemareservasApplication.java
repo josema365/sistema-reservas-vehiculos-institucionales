7270b195ecd60980b7598f2f725331976dc09b0c
Sr.Null <jemanuel599minecraft@gmail.com>
2025-05-22 21:43:28 -0400

    V0.1

---
+++ src/main/java/com/reservas/vehiculos/institucionales/SistemareservasApplication.java
@@ -0,0 +1,13 @@
+package com.reservas.vehiculos.institucionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemareservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemareservasApplication.class, args);
+	}
+
+}
